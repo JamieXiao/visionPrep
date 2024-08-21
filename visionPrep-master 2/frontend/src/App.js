229dce014830387f45bcc17fcd8ea3d0c9d4c974
Jamie Xiao <141690843+JamieXiao@users.noreply.github.com>
2024-08-21 13:59:06 -0400

    Add files via upload

---
+++ visionPrep-master 2/frontend/src/App.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+
+
+function App() {
+  
+  return (
+    <div/>
+    // <div className="App">
+    //   <p>Hi</p>
+    // </div>
+  );
+}
+
+export default App;
